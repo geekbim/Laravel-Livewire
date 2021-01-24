097f77c73d72145cee6eceab6fc53d4f0ce1b070
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2021-01-24 08:28:12 +0700

    Laravel Livewire

--- app/Http/Livewire/ListPost.php
+++ app/Http/Livewire/ListPost.php
@@ -3,10 +3,14 @@
 namespace App\Http\Livewire;
 
 use App\Models\Post;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class ListPost extends Component
 {
+
+    public $updateStateId = 0;
+    public $body = 0;
 
     protected $listeners = [
         'postCreated' => '$refresh',
@@ -19,5 +23,22 @@ class ListPost extends Component
         return view('livewire.list-post', [
             'posts' => $posts,
         ]);
+    }
+
+    public function showUpdateForm($postId)
+    {
+        $post = Post::find($postId);
+        $this->body = $post->body;
+        $this->updateStateId = $postId;
+    }
+
+    public function updatePost($postId)
+    {
+        $post = Post::find($postId);
+        $this->body = $post->body;
+        $post->created_at = Carbon::now();
+        $post->save();
+
+        $this->updateStateId = 0; //biar ketutup lagi textareanya
     }
 }
--- resources/views/livewire/list-post.blade.php
+++ resources/views/livewire/list-post.blade.php
@@ -4,9 +4,31 @@
             <div>
                 <span class="text-xl font-bold">{{ $post->user->name }}</span>
                 <span class="text-gray-600">{{ $post->created_at->diffForHumans() }}</span>
+                <button 
+                    wire:click="showUpdateForm({{ $post->id }})"
+                    class="p-2 bg-purple-600 hover:bg-purple-400 text-white rounded-md"
+                >
+                    Edit
+                </button>
             </div>
             <div>
-                <span>{{ $post->body }}</span>
+                @if($updateStateId !== $post->id)
+                    <span>{{ $post->body }}</span>
+                @endif
+
+                @if($updateStateId === $post->id)
+                <textarea 
+                    wire:model="body"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    rows="3"
+                ></textarea>
+                <button 
+                    wire:click="updatePost({{ $post->id }})"
+                    class="px-4 py-2 bg-blue-600 text-white hover:bg-blue-400 rounded-md"
+                >
+                    Save
+                </button>
+                @endif
             </div>
         </div>
     @endforeach
